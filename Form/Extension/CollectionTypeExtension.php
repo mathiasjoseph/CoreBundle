3f41b610f2fa7967bc5db744a37ed0de58419178
miky <mathias89100@gmail.com>
2017-06-16 02:05:09 +0200

    All

---
+++ Form/Extension/CollectionTypeExtension.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 13/06/17
+ * Time: 00:21
+ */
+
+namespace Miky\Bundle\CoreBundle\Form\Extension;
+
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CollectionTypeExtension extends AbstractTypeExtension
+{
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        // makes it legal for FileType fields to have an image_property option
+        $resolver->setDefined(array('reference_property'));
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        if (isset($options['reference_property'])) {
+            $view->vars['reference_property'] = $options["reference_property"];
+        }else{
+            $view->vars['reference_property'] = null;
+        }
+    }
+
+    /**
+     * Returns the name of the type being extended.
+     *
+     * @return string The name of the type being extended
+     */
+    public function getExtendedType()
+    {
+        // use FormType::class to modify (nearly) every field in the system
+        return CollectionType::class;
+    }
+}
---
+++ Form/Extension/MoneyTypeExtension.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 13/06/17
+ * Time: 00:21
+ */
+
+namespace Miky\Bundle\CoreBundle\Form\Extension;
+
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MoneyTypeExtension extends AbstractTypeExtension
+{
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['currency'] = $options['currency'];
+
+    }
+
+    /**
+     * Returns the name of the type being extended.
+     *
+     * @return string The name of the type being extended
+     */
+    public function getExtendedType()
+    {
+        // use FormType::class to modify (nearly) every field in the system
+        return MoneyType::class;
+    }
+}
