457fff3b9c3720f756da31ab61fc283376cf50bb
miky <mathias89100@gmail.com>
2017-09-28 02:39:33 +0200

    Merge branch 'master' of github.com:mikyprog/Order
    # Conflicts:
    #	.gitignore

--- MikyCoreBundle.php
+++ MikyCoreBundle.php
@@ -2,8 +2,16 @@
 
 namespace Miky\Bundle\CoreBundle;
 
+use Miky\Bundle\CoreBundle\DependencyInjection\Compiler\ShortcodePass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MikyCoreBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new ShortcodePass());
+    }
+
 }
